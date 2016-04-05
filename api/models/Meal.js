4e3cc7c05c512e59ad027bd05701d9b85deb8e71
Valeri Vicneanschi <vicneanschi@gmail.com>
2016-04-05 01:21:45 -0400

    Added meal model

---
+++ api/models/Meal.js
@@ -0,0 +1,40 @@
+/**
+ * Meal.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+      title: {
+          type: "text",
+          required: true
+      },
+      calories: {
+          type: "integer",
+          required: true
+      },
+      createdAtDate: {
+          type: "date",
+          required: true
+      },
+      createdAtTime: {
+          type: "integer",
+          required: true,
+          default: 0
+          
+      },
+      createdBy: {
+          model: 'User',
+          index: true
+      },
+      owner: {
+          model: 'User',
+          index: true
+      }
+
+  }
+};
+
